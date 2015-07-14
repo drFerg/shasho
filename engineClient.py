1205a1db3c06cd27d526396a4e2c0ad043355e76
Fergus <fergus.leahy@gmail.com>
2015-07-14 11:54:35 +0100

    Added setAddr command

--- engineClient.py
+++ engineClient.py
@@ -1,6 +1,9 @@
 import socket
 from struct import pack
 
+CMD_ADDR = 0
+CMD_SPEED = 1
+
 
 class EngineClient():
     """docstring for EngineClient"""
@@ -10,10 +13,13 @@ class EngineClient():
         self.sock = socket.socket(socket.AF_INET,     # Internet
                                   socket.SOCK_DGRAM)  # UDP
 
+    def setAddr(self, addr, newAddr):
+      self.sock.sendto(pack('BHH', CMD_ADDR, addr, newAddr))
+
     def setSpeed(self, addr, speed, direction):
-        self.sock.sendto(pack('HBB', addr, speed, direction),
+        self.sock.sendto(pack('BHBB', CMD_SPEED, addr, speed, direction),
                          (self.addr, self.port))
 
     def setDirection(self, addr, direction):
-        self.sock.sendto(pack('HBB', addr, 0, direction),
+        self.sock.sendto(pack('BHBB', CMD_SPEED, addr, 0, direction),
                          (self.addr, self.port))
